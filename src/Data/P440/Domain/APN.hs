3d834f8e55bb88eddbcfa12e4eb0d503a3a1e3e7
Macil <Macil.dev@gmail.com>
2015-05-14 20:32:46 +0300

    add APN

---
+++ src/Data/P440/Domain/APN.hs
@@ -0,0 +1,43 @@
+module Data.P440.Domain.APN where
+
+import Data.P440.Domain.SimpleTypes
+import Data.P440.Domain.ComplexTypes
+
+import Data.Text (Text)
+
+-- 2.5 Решение НО о икасоовом
+
+data Файл = Файл {
+     идЭС        :: GUID
+    ,типИнф      :: Text
+    ,версПрог    :: Text
+    ,телОтпр     :: Text
+    ,должнОтпр   :: Text
+    ,фамОтпр     :: Text
+    ,версФорм    :: Text
+    ,решенопдпор :: РЕШЕНОПДПОР
+} deriving (Eq, Show)
+
+data РЕШЕНОПДПОР = РЕШЕНОПДПОР {
+     номРеш       :: Text
+    ,датаПодп     :: Date
+    ,банкПл       :: Text
+    ,бикбПл       :: БИК
+    ,инннп        :: Text
+    ,кппнп        :: Maybe КПП
+    ,приостПоруч  :: [ПриостПоруч]
+    ,счета        :: [Счет]
+    ,свНО         :: СвНО
+    ,руководитель :: РукНО
+} deriving (Eq, Show)
+
+data ПриостПоруч = ПриостПоруч {
+     номПоруч  :: Text
+    ,датаПоруч :: Date
+    ,видПоруч  :: Text
+} deriving (Eq, Show)
+
+data Счет = Счет {
+     номСчПл :: Text
+    ,видСч   :: Text
+} deriving (Eq, Show)
