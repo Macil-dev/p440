81055d48c4197b12c20134e4a950a74fae57c781
Macil <Macil.dev@gmail.com>
2015-05-14 20:55:35 +0300

    add APZ

---
+++ src/Data/P440/Domain/APZ.hs
@@ -0,0 +1,44 @@
+module Data.P440.Domain.APZ where
+
+import Data.P440.Domain.SimpleTypes
+import Data.P440.Domain.ComplexTypes
+
+import Data.Text (Text)
+
+-- 2.7 Отзыв инкассового поручения
+
+data Файл = Файл {
+     идЭС        :: GUID
+    ,типИнф      :: Text
+    ,версПрог    :: Text
+    ,телОтпр     :: Text
+    ,должнОтпр   :: Text
+    ,фамОтпр     :: Text
+    ,версФорм    :: Text
+    ,решенотзпор :: РЕШЕНОТЗПОР
+} deriving (Eq, Show)
+
+data РЕШЕНОТЗПОР = РЕШЕНОТЗПОР {
+     номРеш       :: Text
+    ,датаПодп     :: Date
+    ,банкПл       :: Text
+    ,бикбПл       :: БИК
+    ,инннп        :: Text
+    ,кппнп        :: Maybe КПП
+    ,сумма        :: Maybe Text
+    ,отозвПоруч   :: [ОтозвПоруч]
+    ,счета        :: [Счет]
+    ,свНО         :: СвНО
+    ,руководитель :: РукНО
+} deriving (Eq, Show)
+
+data ОтозвПоруч = ОтозвПоруч {
+     номПоруч  :: Text
+    ,датаПоруч :: Date
+    ,видПоруч  :: Text
+} deriving (Eq, Show)
+
+data Счет = Счет {
+     номСчПл :: Text
+    ,видСч   :: Text
+} deriving (Eq, Show)
