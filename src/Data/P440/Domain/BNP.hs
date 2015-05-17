13d8cd2832cb128392c2c82e817815ed7be62b03
Macil <Macil.dev@gmail.com>
2015-05-17 15:45:10 +0300

    add BNP

---
+++ src/Data/P440/Domain/BNP.hs
@@ -0,0 +1,45 @@
+module Data.P440.Domain.BNP where
+
+import Data.P440.Domain.SimpleTypes
+import Data.P440.Domain.ComplexTypes
+
+import Data.Text (Text)
+
+-- 2.16 Исполнение о неисполнении/частичном испонении PNO
+
+data Файл = Файл {
+     идЭС       :: GUID
+    ,типИнф     :: Text
+    ,версПрог   :: Text
+    ,телОтпр    :: Text
+    ,должнОтпр  :: Text
+    ,фамОтпр    :: Text
+    ,версФорм   :: Text
+    ,сбщбннеисп :: СБЩБННЕИСП
+} deriving (Eq, Show)
+
+data СБЩБННЕИСП = СБЩБННЕИСП {
+     номСообщ  :: Text
+    ,номПоруч  :: Text
+    ,датаПоруч :: Date
+    ,кодНОБ    :: ИФНС
+    ,сумПлат   :: Text
+    ,инннп     :: Maybe Text
+    ,кппнп     :: Maybe КПП
+    ,плательщ  :: Text
+    ,кодИспол  :: Text
+    ,сумЧаст   :: Maybe Text
+    ,допСвед   :: Maybe Text
+    ,датаПодп  :: Date
+    ,номСчПлИлиИдКЭСП :: НомСчИлиИдКЭСП
+    ,свБанкИлиСвУБР   :: СвБанкИлиСвУБР
+    ,предБанка :: ПредБ
+} deriving (Eq, Show )
+
+data НомСчИлиИдКЭСП = НомСчПл НомСч
+                    | ИдКЭСП Text
+                    deriving (Eq, Show)
+
+data СвБанкИлиСвУБР = СвБанк Банк
+                    | СвУБР УБР
+                    deriving (Eq, Show)
